d7d3dbe19a4095584b8885dad00e463c2755876b
KayakCat <lmhall@email.neit.edu>
2024-02-06 18:56:45 -0500

    got lab

--- week4/GOT.py
+++ week4/GOT.py
@@ -11,6 +11,9 @@ age = []
 nname = []
 alleg = []
 
+allegiances = ['Stark', 'Baratheon','Tully','Nights Watch', 'Lannister', 'Targaryen']
+mottos = ['Winter is Coming', 'Ours is the fury', 'Family. Duty. Honor.', 'And now my watch begins.' 'Hear Me Roar!', 'Fire and Blood']   
+
 with open ("week4/files/lab4A_GOT_NEW.txt") as csvfile:
 
     file = csv.reader(csvfile)
@@ -27,6 +30,43 @@ with open ("week4/files/lab4A_GOT_NEW.txt") as csvfile:
 print(f"{'FIRST':12} \t {'LAST':12} \t {'AGE':3} \t {'NICKNAME':18} \t {'HOUSE':15}")
 print("--------------------------------------------------------------------------------")
 
-for i in range(0,len(fname)):
+for i in range(0, len(fname)):
 
     print(f"{fname[i]:12} \t {lname[i]:12} \t {age[i]:3} \t {nname[i]:18} \t {alleg[i]:15}")
+
+
+print(f"{'FIRST':12} \t {'LAST':12} \t {'AGE':3} \t {'NICKNAME':18} \t {'HOUSE':15} \t{'MOTTO':25}")
+print("---------------------------------------------------------------------------------------------------")
+
+for i in range(0, len(fname)):
+    house = alleg[i]
+    motto = allegiances
+
+    if house == "Stark":
+        motto = "Winter is Coming"
+
+    elif house == "Baratheon":
+        motto = "Ours is the fury"
+
+    elif house == "Tully":
+        motto = "Family. Duty. Honor."
+
+    elif house == "Night's Watch":
+        motto = "And now my watch begins."
+
+    elif house == "Lannister":
+        motto = "Hear Me Roar"
+
+    elif house == "Targaryen":
+        motto = "Fire and Blood"
+
+    else:
+        motto = "UNKNOWN MOTTO"
+
+
+    print(f"{fname[i]:12} \t {lname[i]:12} \t {age[i]:3} \t {nname[i]:18} \t {alleg[i]:15} \t {motto:25}")
+
+
+
+    
+
